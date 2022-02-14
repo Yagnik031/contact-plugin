05b0590e0ea0d86744e40a484b51d6540fbaaef5
Yagnik031 <90082946+Yagnik031@users.noreply.github.com>
2022-02-14 15:20:25 +0530

    ff

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,6 @@
 export interface ContactsPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+
+
+  getContacts(filter: string): Promise<{ results: any[] }>;
 }
--- src/web.ts
+++ src/web.ts
@@ -3,6 +3,9 @@ import { WebPlugin } from '@capacitor/core';
 import type { ContactsPlugin } from './definitions';
 
 export class ContactsWeb extends WebPlugin implements ContactsPlugin {
+  getContacts(_filter: string): Promise<{ results: any[]; }> {
+    throw new Error('Method not implemented.');
+  }
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
