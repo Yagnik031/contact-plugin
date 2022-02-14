8e1242296c6c7a5d77fe479f48408a83039224c7
Yagnik031 <90082946+Yagnik031@users.noreply.github.com>
2022-02-14 15:29:37 +0530

    ff

--- src/web.ts
+++ src/web.ts
@@ -1,11 +1,18 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { ContactsPlugin } from './definitions';
+import type { Contact, ContactsPlugin, NewContact, PermissionStatus } from './definitions';
 
 export class ContactsWeb extends WebPlugin implements ContactsPlugin {
-  getContacts(_filter: string): Promise<{ results: any[]; }> {
+  getPermissions(): Promise<PermissionStatus> {
     throw new Error('Method not implemented.');
   }
+  getContacts(): Promise<{ contacts: Contact[]; }> {
+    throw new Error('Method not implemented.');
+  }
+  saveContact(_contact: NewContact): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
